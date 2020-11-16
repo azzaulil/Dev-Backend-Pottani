e4140a80b9e1c7e6c5543ee07ca58f1990c4a0fb
azzaulil <azzaulil50@gmail.com>
2020-11-17 01:13:29 +0700

    add response

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -91,7 +91,7 @@ class AuthController extends Controller
                         'status' => 'Success',
                         'token' => $tokenResult->accessToken,
                         'id_role' =>array_values($users)[0]['id_role'],
-                        'email' => array_values($users)[0]['email'],
+                        'nama_lengkap' => array_values($members)[0]['nama_lengkap'],
                         // 'foto_profil'=>array_values($members)[0]['image_URL'],
                     ]);
                 
@@ -100,7 +100,7 @@ class AuthController extends Controller
                         'status' => 'Success',
                         'id_role' => 1,
                         'token' => $tokenResult->accessToken,
-                        'role' => 'admin'
+                        'role_name' => 'Admin'
                     ]);
                 }
            }else if(Auth::user()->is_active == 0){
