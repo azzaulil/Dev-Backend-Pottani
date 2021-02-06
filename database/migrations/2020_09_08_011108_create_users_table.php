323640cc12ab754c2027f2a1364132b05876f6d3
azzaulil <azzaulil50@gmail.com>
2021-02-06 16:29:41 +0700

    migrate update

--- database/migrations/2020_09_08_011108_create_users_table.php
+++ database/migrations/2020_09_08_011108_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->boolean('is_active')->default(0);
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('verified_token');
             $table->rememberToken();
             $table->timestamps();
 
